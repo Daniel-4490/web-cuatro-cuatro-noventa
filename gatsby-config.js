a7220e2622124f9455628093f4f1c5fe432dae24
Daniel Alarcón <daniel.alarcon.1992@gmail.com>
2021-03-17 20:58:27 -0300

    Cambie gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,46 @@
 module.exports = {
   siteMetadata: {
-    title: "cuatrocuatronoventa",
+    title: 'Cuatro Cuatro Noventa',
+    description: 'En constante adaptación',
+    author: 'Daniel Alarcón',
+    siteUrl: 'https://cuatrocuatronoventa.com'
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
     'gatsby-plugin-postcss',
     // {
-    //   resolve: "gatsby-plugin-google-analytics",
+    //   resolve: 'gatsby-plugin-google-analytics',
     //  options: {
-    //    trackingId: "",
+    //    trackingId: ',
     //  },
-    //},
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    // }
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
-      },
+        icon: 'src/images/icon.png'
+      }
     },
-    "gatsby-transformer-remark",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/'
       },
-      __key: "images",
+      __key: 'images'
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/'
       },
-      __key: "pages",
-    },
-  ],
-};
+      __key: 'pages'
+    }
+  ]
+}
